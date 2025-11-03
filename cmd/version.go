98557942d83780869a10f11c26caa86b166e359d
Owen Easter <owen@easter.company>
2025-11-03 07:28:40 +0000

    fix: Ignore error from resp.Body.Close in isOfficialRelease

--- cmd/version.go
+++ cmd/version.go
@@ -59,7 +59,7 @@ func isOfficialRelease(currentVersion string) bool {
 	if err != nil {
 		return false
 	}
-	defer resp.Body.Close()
+	defer func() { _ = resp.Body.Close() }()
 
 	if resp.StatusCode != http.StatusOK {
 		return false
