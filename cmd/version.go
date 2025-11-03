ef326727d68898fc09387eec3e37564c65bbb512
Owen Easter <owen@easter.company>
2025-11-03 05:17:14 +0000

    style: Refine version command output

--- cmd/version.go
+++ cmd/version.go
@@ -5,10 +5,15 @@ import (
 	"runtime"
 	"strings"
 
-	"github.com/EasterCompany/dex-cli/ui"
+	"github.com/charmbracelet/lipgloss"
 )
 
 func Version(version, branch, commit, buildDate, buildYear string) {
+	// Define styles
+	darkStyle := lipgloss.NewStyle().Foreground(lipgloss.Color("240"))    // Dark grey
+	brightStyle := lipgloss.NewStyle().Foreground(lipgloss.Color("86"))   // Bright purple
+	dividerStyle := lipgloss.NewStyle().Foreground(lipgloss.Color("240")) // Dark grey for the dividers
+
 	// Format the build date: 2025-11-03T02:38:13Z -> 2025-11-03-02-38-13
 	formattedDate := strings.ReplaceAll(buildDate, "T", "-")
 	formattedDate = strings.ReplaceAll(formattedDate, ":", "-")
@@ -26,11 +31,12 @@ func Version(version, branch, commit, buildDate, buildYear string) {
 		arch,
 	)
 
-	// Assemble the final output
-	versionString := fmt.Sprintf(
-		"dex-cli | %s | Easter Company™ © %s",
-		fullVersion,
-		buildYear,
-	)
-	ui.PrintInfo(versionString)
+	// Assemble the styled parts
+	part1 := darkStyle.Render("dex-cli")
+	part2 := brightStyle.Render(fullVersion)
+	part3 := darkStyle.Render(fmt.Sprintf("Easter Company™ © %s", buildYear))
+	divider := dividerStyle.Render("|")
+
+	// Print the final, styled string
+	fmt.Printf("%s %s %s %s %s\n", part1, divider, part2, divider, part3)
 }
