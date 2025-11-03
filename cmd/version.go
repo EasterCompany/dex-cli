599d31ca5dfe49c7f79d96ede49c4a4262784b2e
Owen Easter <owen@easter.company>
2025-11-03 05:20:27 +0000

    style: Re-apply color formatting to version output

--- cmd/version.go
+++ cmd/version.go
@@ -4,9 +4,14 @@ import (
 	"fmt"
 	"runtime"
 	"strings"
+
+	"github.com/charmbracelet/lipgloss"
 )
 
 func Version(version, branch, commit, buildDate, buildYear string) {
+	// Style for the trademark part
+	darkStyle := lipgloss.NewStyle().Foreground(lipgloss.Color("240")) // Dark grey
+
 	// Format the build date: 2025-11-03T02:38:13Z -> 2025-11-03-02-38-13
 	formattedDate := strings.ReplaceAll(buildDate, "T", "-")
 	formattedDate = strings.ReplaceAll(formattedDate, ":", "-")
@@ -24,6 +29,9 @@ func Version(version, branch, commit, buildDate, buildYear string) {
 		arch,
 	)
 
+	// Style the trademark part
+	trademarkPart := darkStyle.Render(fmt.Sprintf("| Easter Company™ © %s", buildYear))
+
 	// Assemble and print the final output
-	fmt.Printf("%s | Easter Company™ © %s\n", fullVersion, buildYear)
+	fmt.Printf("%s %s\n", fullVersion, trademarkPart)
 }
