ede11c8156f7860d11483b78ba10db4c5275bff2
Owen Easter <owen@easter.company>
2025-11-05 08:46:41 +0000

    dex build: successful build

--- cmd/event.go
+++ cmd/event.go
@@ -1,25 +1,15 @@
 package cmd
 
 import (
-	"fmt"
-
-	"github.com/EasterCompany/dex-cli/config"
 	"github.com/EasterCompany/dex-cli/ui"
-	"github.com/EasterCompany/dex-cli/utils"
 )
 
 // Event provides commands to interact with the dex-event-service
 func Event(args []string) error {
-	def, err := config.Resolve("event")
-	if err != nil {
-		return err
-	}
-
-	status, _, err := utils.GetHTTPBody(def.GetHTTP("/service"))
-	if err != nil {
-		return fmt.Errorf("failed to get event service status: %w", err)
-	}
-
-	ui.PrintCodeBlockFromBytes(status, "event-service", "json")
+	// Simple test to see if colors render at all.
+	ui.PrintInfo("--- ANSI Color Test ---")
+	ui.PrintRaw("\x1b[32mThis line should be green.\x1b[0m\n")
+	ui.PrintRaw(ui.Colorize("This line should also be green.", ui.ColorGreen) + "\n")
+	ui.PrintInfo("--- End Test ---")
 	return nil
 }
