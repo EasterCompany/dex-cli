acfb3cfc12b9946b319e9436dae22d1e6fc5e13c
Owen Easter <owen@easter.company>
2025-11-05 05:50:09 +0000

    dex build: successful build

--- utils/summary.go
+++ utils/summary.go
@@ -3,18 +3,41 @@ package utils
 import (
 	"fmt"
 	"os"
+	"strings"
 
 	"github.com/EasterCompany/dex-cli/config"
 	"github.com/EasterCompany/dex-cli/ui"
 )
 
+// truncateVersion shortens a version string for display.
+func truncateVersion(version string) string {
+	// Don't truncate special status strings
+	if version == "N/A" || version == "OK" || version == "BAD" {
+		return version
+	}
+
+	parts := strings.Split(version, ".")
+	if len(parts) >= 3 {
+		return strings.Join(parts[:3], ".")
+	}
+
+	if len(version) > 5 {
+		return version[:5]
+	}
+
+	return version
+}
+
 // FormatSummaryLine formats a single line for the update summary.
 func FormatSummaryLine(service config.ServiceDefinition, oldVersion, newVersion string, oldSize, newSize int64) string {
+	displayOld := truncateVersion(oldVersion)
+	displayNew := truncateVersion(newVersion)
+
 	var versionChange string
 	if oldVersion == newVersion {
-		versionChange = ui.Colorize(newVersion, ui.ColorDarkGray)
+		versionChange = ui.Colorize(displayNew, ui.ColorDarkGray)
 	} else {
-		versionChange = ui.Colorize(fmt.Sprintf("%s -> %s", oldVersion, newVersion), ui.ColorGreen)
+		versionChange = ui.Colorize(fmt.Sprintf("%s -> %s", displayOld, displayNew), ui.ColorGreen)
 	}
 
 	var sizeChange string
