fe7812445f7f79bceab9252844ebf0679c271349
Owen Easter <owen@easter.company>
2025-11-05 00:36:36 +0000

    dex build: successful build

--- main.go
+++ main.go
@@ -123,7 +123,9 @@ func main() {
 		runCommand(func() error { return cmd.Config(os.Args[2:]) })
 
 	case "help", "-h", "--help":
+		fmt.Println() // Add padding at the start
 		printUsage()
+		fmt.Println() // Add padding at the end
 
 	default:
 		ui.PrintError(fmt.Sprintf("Unknown command: %s", command))
